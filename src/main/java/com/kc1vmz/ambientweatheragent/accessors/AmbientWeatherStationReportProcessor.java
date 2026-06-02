2aaf68cb152ed8805124982c377244ecfe31a812
John Rokicki <jcrokicki@gmail.com>
2026-06-02 13:32:18 -0400

    change to offload CWOP send to a separate queue/thread

--- src/main/java/com/kc1vmz/ambientweatheragent/accessors/AmbientWeatherStationReportProcessor.java
+++ src/main/java/com/kc1vmz/ambientweatheragent/accessors/AmbientWeatherStationReportProcessor.java
@@ -1,11 +1,31 @@
 package com.kc1vmz.ambientweatheragent.accessors;
 
+/*
+    Copyright (c) 2026 John Rokicki KC1VMZ
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+    http://www.kc1vmz.com
+*/
+
 import java.time.ZonedDateTime;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.kc1vmz.ambientweatheragent.objects.AmbientWeatherStationProperties;
 import com.kc1vmz.ambientweatheragent.objects.CWOPReport;
+import com.kc1vmz.ambientweatheragent.queue.CWOPReportQueue;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -13,7 +33,7 @@ import jakarta.inject.Singleton;
 @Singleton
 public class AmbientWeatherStationReportProcessor {
     @Inject
-    private CWOPReportSenderTCPIP cwopReportSenderTCPIP;
+    private CWOPReportQueue cwopReportQueue;
     private static final Logger logger = LogManager.getLogger(AmbientWeatherStationReportProcessor.class);
 
     /**
@@ -22,7 +42,7 @@ public class AmbientWeatherStationReportProcessor {
      */
     public void processReport(AmbientWeatherStationProperties report) {
         try {
-            cwopReportSenderTCPIP.sendToCWOP(createCWOPReport(report));
+            cwopReportQueue.addCWOPReport(createCWOPReport(report));
         } catch (Exception e) {
            logger.error("Exception caught", e);
         }
---
+++ src/main/java/com/kc1vmz/ambientweatheragent/queue/CWOPReportQueue.java
@@ -0,0 +1,69 @@
+package com.kc1vmz.ambientweatheragent.queue;
+
+/*
+    Copyright (c) 2026 John Rokicki KC1VMZ
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+    http://www.kc1vmz.com
+*/
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.kc1vmz.ambientweatheragent.objects.CWOPReport;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class CWOPReportQueue {
+
+    private static final Logger logger = LogManager.getLogger(CWOPReportQueue.class);
+    private static final int QUEUE_SIZE=10;
+
+    private ArrayBlockingQueue<CWOPReport> queue = null;
+    private boolean stop = false;
+
+    private void initializeQueue() {
+        if (queue == null) {
+            queue = new ArrayBlockingQueue<>(QUEUE_SIZE);
+        }
+    }
+    public synchronized ArrayBlockingQueue<CWOPReport> getQueue() {
+        initializeQueue();
+        return queue;
+    }
+    public synchronized void addCWOPReport(CWOPReport obj) {
+        if (!stayRunning()) {
+            return;
+        }
+        ArrayBlockingQueue<CWOPReport> initializedQueue = getQueue();
+        try {
+            initializedQueue.add(obj);
+        } catch (IllegalStateException e) {
+            logger.error("IllegalStateException adding to queue", e);
+            shutdown();
+        } catch (Exception e) {
+            logger.error("Exception adding to queue", e);
+        }
+    }
+    public boolean stayRunning() {
+        return !stop;
+    }
+    public void shutdown() {
+        stop = true;
+    }
+}
